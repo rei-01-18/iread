403f4815aa0626b91dcc9856c106a495bbc51c4a
rei-01-18 <80232197+rei-01-18@users.noreply.github.com>
2021-07-08 23:04:10 +0900

    コメントの削除機能

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,15 @@ class CommentsController < ApplicationController
     redirect_to book_path(book.id)
   end
 
+  def destroy
+    @book = Book.find(params[:book_id])
+    @comment = @book.Comment.find_by(params[:id])
+    redirect_to book_path(book.id)
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(book_id: params[:book_id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'books#index'
   resources :books, only: [:index, :new, :create, :destroy, :show] do
-    resources :comments, only: :create
+    resources :comments, only: [:create, :destroy]
   end
 end
