8ae60a60115a8b2275a7065524d6b909c6a0e702
rei-01-18 <80232197+rei-01-18@users.noreply.github.com>
2021-07-09 11:36:27 +0900

    削除機能　修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,23 @@
 class CommentsController < ApplicationController
+  before_action :set_book
+
   def create
-    @comment = Comment.create(comment_params)
+    @comment = @book.Comment.create(comment_params)
     redirect_to book_path(book.id)
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
-    @comment = @book.Comment.find_by(params[:id])
-    redirect_to book_path(book.id)
+    @comment = Comment.find(params[:id]).destroy
+    redirect_to book_path(@book)
   end
 
   private
   def comment_params
     params.require(:comment).permit(:text).merge(book_id: params[:book_id])
+  end
+
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 
 end
